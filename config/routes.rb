29780fab97735fcbbb4b0dd4ee1f548398b3b92a
Auriga <area51alienware@gmail.com>
2016-06-19 11:37:40 -0700

    Added files. Finished. Just needs some work

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :comments
+  get 'home/index'
+
+  devise_for :users
+  resources :posts do 
+    member do
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+    end
+    resources :comments
+  end
+  root to: "posts#index"
+  
+end
