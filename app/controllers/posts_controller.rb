ae15f8933cf67708a96dfda4852526ddadb1c842
Auriga <AG-Systems@users.noreply.github.com>
2016-10-28 18:18:42 -0700

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,7 +76,7 @@ class PostsController < ApplicationController
     redirect_to :back
   end
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_post
       @post = Post.find(params[:id])
     end
